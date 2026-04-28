a797278e43bdb234e7b34087ff13e4edfe436ad0
MaxiRe115 <danielmaxi115@gmail.com>
2026-04-27 21:56:02 -0500

    3.33

--- todo_manager.py
+++ todo_manager.py
@@ -16,7 +16,10 @@ def read_todo_file(file_path):
 def write_todo_file(file_path, tasks):
     """Writes tasks to a file, one per line."""
     # TODO: Implementar escritura de tareas según README.md
-    with open(file_path, 'w') as file:
-        for task in tasks:
-            file.write(task + "\n")
+    try:
+        with open(file_path, 'w') as file:
+            for task in tasks:
+                file.write(task + "\n")
+    except Exception:
+        print(f"Error writing to file {file_path}.")
     raise NotImplementedError
